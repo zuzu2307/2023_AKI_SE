bc0ba6eec84def1dd4ca57d1d2b995b22bb0154e
Vee <korntawat.dev@gmail.com>
2023-10-07 12:04:17 +0900

    change source and target string

--- Assignment_2_Levenshtein_Distance/2.py
+++ Assignment_2_Levenshtein_Distance/2.py
@@ -13,14 +13,14 @@ def search(source, target, s_lenght, t_lenght):
     if source[s_lenght - 1] == target[t_lenght - 1]:
         return search(source, target, s_lenght - 1, t_lenght - 1)
 
-    # return minimum of distance between 3 type of edit DELETE, ADD, CHANGE and + 1 for action
+    # return minimum of distance between 3 type of edit DELETE, ADD, CHANGE and + 1 for an action
     return 1 + min(search(source, target, s_lenght - 1, t_lenght), min(search(source, target, s_lenght, t_lenght - 1), search(source, target, s_lenght - 1, t_lenght - 1)))
 
 
-source = 'I am father'
-target = 'I am mother'
+source = 'みねりゅうた'
+target = 'りゅうらいた'
 
-# Compare between library and created function
+# compare between library and created function
 lib_distance = Levenshtein.distance(source, target)
 distance = search(source, target, len(source), len(target))
 
