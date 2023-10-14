99cf4ff5b30350721cefb535e9742f6f4235f954
Vee <korntawat.dev@gmail.com>
2023-10-14 12:11:08 +0900

    adding comment

--- Assigment_3_Index_Structure/3.py
+++ Assigment_3_Index_Structure/3.py
@@ -5,19 +5,24 @@ news = fetch_20newsgroups(subset='all')
 
 news_data = news.data
 
+# split all data in to a word and make all lower for searching easily
+
 
 def simplify(data):
     return data.lower().split()
 
 
+# simplify data
 document = [simplify(doc) for doc in news_data]
 
+# making a dict set to match number and word then use it to make a set of word from datas
 index_set = defaultdict(set)
 for doc_id, words in enumerate(document):
     for word in words:
         index_set[word].add(doc_id)
 
 
+# searching a word in data_set
 def search(keyword, data_set):
     return data_set[keyword]
 
