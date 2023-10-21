c571d8c1d3007e887c81afffc2e78c18c200c614
Korntawat Witchuvanit <korntawat.dev@gmail.com>
2023-10-21 14:09:23 +0900

    finish assignment4

--- Assignment_4_Vector_Space_Model/4.py
+++ Assignment_4_Vector_Space_Model/4.py
@@ -1,15 +1,37 @@
 from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
-data = fetch_20newsgroups(subset="all")
+news = fetch_20newsgroups(subset="all")
 
-texts = ["文字列のリスト"]
-query = ["文字列"]
+print(f"{len(news.data)} news")
 
-vectorizer = CountVectorizer()
-X = vectorizer.fit_transform(texts)
-q = vectorizer.transform(query)
+# create a tf-idf vectorizer object and transform the news
+vectorizer = TfidfVectorizer(stop_words='english')
+doc_vectors = vectorizer.fit_transform(news.data)
 
-print(X,q)
+# using first document to find a similar
+input_index = 0
+input_doc_vector = doc_vectors[input_index]
 
+# checking similarity between the input document vector and all document vectors
+cosine_similarities = cosine_similarity(
+    input_doc_vector, doc_vectors).flatten()
+
+# sorting documents based on their cosine similarity to the input document and get the most similar documents
+related_docs_indices = cosine_similarities.argsort()[
+    ::-1]
+
+# limit showing document
+limit = 5
+
+print(f"\nMost {limit} similar documents to document #{input_index}:")
+# Skipping the first result since it's the input document itself
+for i, index in enumerate(related_docs_indices[1:], start=1):
+    if i > limit:  # Let's show top 5 matches
+        break
+    print("---------------------------------------------------------------------------------")
+    print(
+        f"Document #{index} with similarity score: {cosine_similarities[index]:.2f}")
+    print("\nContent:\n", news.data[index], "\n")
