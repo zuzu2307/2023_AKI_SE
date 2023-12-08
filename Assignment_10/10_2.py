c4172b18757ff6119541e52d669ec4902ebf814a
Korntawat Witchuvanit <korntawat.wc@gmail.com>
2023-12-08 17:56:48 +0900

    clean code

--- Assignment_10/10_2.py
+++ Assignment_10/10_2.py
@@ -1,15 +1,7 @@
 import matplotlib.pyplot as plt
-import os
-import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.linear_model import Ridge
-from sklearn.preprocessing import PolynomialFeatures
 from sklearn.ensemble import RandomForestRegressor
 import mglearn
 import pandas as pd
-
 
 def eval_on_features(features, target, regressor):
     X_train, X_test = features[:n_train], features[n_train:]
@@ -22,7 +14,7 @@ def eval_on_features(features, target, regressor):
     y_pred_train = regressor.predict(X_train)
 
     plt.figure(figsize=(10,3)) 
-    xticks = pd.date_range(start=0,end=len(X_hour),freq="D")
+    xticks = pd.date_range(start=citibike.index.min(),end=citibike.index.max(),freq="D")
     plt.xticks(xticks,xticks.strftime("%a %m-%d"), rotation = 90, ha = "left")
 
     plt.plot(range(n_train), y_train, label="train")
@@ -37,11 +29,11 @@ def eval_on_features(features, target, regressor):
     plt.show()
 
 
-
 citibike = mglearn.datasets.load_citibike().resample("1h").sum().fillna(0)
 y = citibike.values
 regressor = RandomForestRegressor(n_estimators=100,random_state=0)
 X_hour = citibike.index.hour.values.reshape(-1, 1)
 n_train = 184
+
 
 eval_on_features(X_hour,y,regressor)
