b77ab0230f92f86f4f75115423fec636815bb209
Korntawat Witchuvanit <korntawat.wc@gmail.com>
2023-12-19 13:03:04 +0900

    11.2

--- Assignment_11/11_2.py
+++ Assignment_11/11_2.py
@@ -1,49 +1,38 @@
-from sklearn.datasets import load_iris
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_curve, RocCurveDisplay
 from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
 
+# Generate a binary classification dataset
+X, y = make_classification(n_samples=1000, n_features=20, n_classes=2, random_state=42)
 
-# Load the Iris dataset
-iris = load_iris()
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Define the pipeline
-pipe = Pipeline([
-    ('scaler', StandardScaler()), 
-    ('classifier', SVC())
-])
+# Train a Support Vector Machine (SVM) classifier
+svm = SVC(probability=True, random_state=42)
+svm.fit(X_train, y_train)
+y_pred_svm = svm.predict_proba(X_test)[:, 1]
 
+# Train a Random Forest classifier
+rf = RandomForestClassifier(random_state=42)
+rf.fit(X_train, y_train)
+y_pred_rf = rf.predict_proba(X_test)[:, 1]
 
-# Create a parameter grid
-param_grid = [
-    {
-        'classifier': [SVC()],
-        'classifier__C': [1, 10, 100],
-        'classifier__gamma': [0.001, 0.01, 0.1],
-        'scaler': [StandardScaler(), None]
-    },
-    {
-        'classifier': [KNeighborsClassifier()],
-        'classifier__n_neighbors': [3, 5, 7],
-        'scaler': [StandardScaler(), None]
-    }
-]
+# Calculate ROC curves
+fpr_svm, tpr_svm, _ = roc_curve(y_test, y_pred_svm)
+fpr_rf, tpr_rf, _ = roc_curve(y_test, y_pred_rf)
 
+# Plot ROC curves in a single figure for comparison
+plt.figure(figsize=(8, 6))
+plt.plot(fpr_svm, tpr_svm, label='SVM')
+plt.plot(fpr_rf, tpr_rf, label='Random Forest')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curves Comparison: SVM vs Random Forest')
+plt.legend()
+plt.show()
 
-X_train, X_test, y_train, y_test = train_test_split(
- iris.data, iris.target, random_state=0)
-
-
-# Instantiate GridSearchCV
-grid_search = GridSearchCV(pipe, param_grid, cv=5)
-
-# Fit the GridSearchCV Model
-grid_search.fit(X_train, y_train)
-
-
-print("Best params:\n{}\n".format(grid_search.best_params_))
-print("Best cross-validation score: {:.2f}".format(grid_search.best_score_))
-print("Test-set score: {:.2f}".format(grid_search.score(X_test, y_test)))
 
